5c14a544895de23f2aa46ad54d4bfda7e949cb80
esp32-urobot <urobot2011@gmail.com>
2023-08-20 20:52:01 +0900

    Update ReadTouchParam.ino

--- examples/ReadTouchParam/ReadTouchParam.ino
+++ examples/ReadTouchParam/ReadTouchParam.ino
@@ -4,36 +4,24 @@
 #define I2C_SCL 23
 #define RST_N_PIN 21
 #define INT_N_PIN 34
+#define DISPLAY_WIDTH 135
+#define DISPLAY_HEIGHT 240
 
-FT6336U ft6336u(I2C_SDA, I2C_SCL, RST_N_PIN, INT_N_PIN);
+Touch ctp = Touch(DISPLAY_HEIGHT, DISPLAY_WIDTH, I2C_SDA, I2C_SCL, RST_N_PIN, INT_N_PIN);
 
 void setup() {
     Serial.begin(115200);
 
-    ft6336u.begin();
-
-    Serial.print("FT6336U Firmware Version: ");
-    Serial.println(ft6336u.read_firmware_id());
-    Serial.print("FT6336U Device Mode: ");
-    Serial.println(ft6336u.read_device_mode());
+    ctp.begin();
 }
 
 void loop() {
-    if(digitalRead(INT_N_PIN) != -1) {
-        Serial.print("FT6336U TD Status: ");
-        Serial.println(ft6336u.read_td_status());
-        Serial.print("FT6336U Touch Event/ID 1: (");
-        Serial.print(ft6336u.read_touch1_event()); Serial.print(" / "); Serial.print(ft6336u.read_touch1_id()); Serial.println(")");
-        Serial.print("FT6336U Touch Position 1: (");
-        Serial.print(ft6336u.read_touch1_x()); Serial.print(" , "); Serial.print(ft6336u.read_touch1_y()); Serial.println(")");
-        Serial.print("FT6336U Touch Weight/MISC 1: (");
-        Serial.print(ft6336u.read_touch1_weight()); Serial.print(" / "); Serial.print(ft6336u.read_touch1_misc()); Serial.println(")");
-        Serial.print("FT6336U Touch Event/ID 2: (");
-        Serial.print(ft6336u.read_touch2_event()); Serial.print(" / "); Serial.print(ft6336u.read_touch2_id()); Serial.println(")");
-        Serial.print("FT6336U Touch Position 2: (");
-        Serial.print(ft6336u.read_touch2_x()); Serial.print(" , "); Serial.print(ft6336u.read_touch2_y()); Serial.println(")");
-        Serial.print("FT6336U Touch Weight/MISC 2: (");
-        Serial.print(ft6336u.read_touch2_weight()); Serial.print(" / "); Serial.print(ft6336u.read_touch2_misc()); Serial.println(")");
+    if(ctp.touched()){
+        TS_Point p = ctp.getPoint();
+        Serial.print("Touch sensing: ");
+        Serial.print(p.x);
+        Serial.print(", ");
+        Serial.print(p.y);
+        Serial.println(".");
     }
-
 }
