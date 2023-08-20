e7021ad14c581ba57e0b673e1971236a33ba453f
esp32-urobot <urobot2011@gmail.com>
2023-08-20 19:39:30 +0900

    Update ScanMultiTouch.ino

--- examples/ScanMultiTouch/ScanMultiTouch.ino
+++ examples/ScanMultiTouch/ScanMultiTouch.ino
@@ -6,7 +6,10 @@
 #define RST_N_PIN 21
 #define INT_N_PIN 34
 
-FT6336U ft6336u(I2C_SDA, I2C_SCL, RST_N_PIN, INT_N_PIN);
+#define DISPLAY_WIDTH 135
+#define DISPLAY_HEIGHT 240
+
+Touch ft6336u = Touch(DISPLAY_HEIGHT, DISPLAY_WIDTH, I2C_SDA, I2C_SCL, RST_N_PIN, INT_N_PIN);
 
 void setup() {
     Serial.begin(115200);
@@ -15,42 +18,42 @@ void setup() {
 
 //    ft6336u.write_device_mode(factory_mode);
     Serial.print("FT6336U Device Mode: ");
-    Serial.println(ft6336u.read_device_mode());
+    Serial.println(ft6336u.ft6336u.read_device_mode());
     Serial.print("FT6336U Threshold: 0x");
-    Serial.println(ft6336u.read_touch_threshold(), HEX);
+    Serial.println(ft6336u.ft6336u.read_touch_threshold(), HEX);
     Serial.print("FT6336U Filter Coefficient: 0x");
-    Serial.println(ft6336u.read_filter_coefficient(), HEX);
+    Serial.println(ft6336u.ft6336u.read_filter_coefficient(), HEX);
     Serial.print("FT6336U Control Mode: 0x");
-    Serial.println(ft6336u.read_ctrl_mode(), HEX);
+    Serial.println(ft6336u.ft6336u.read_ctrl_mode(), HEX);
     Serial.print("FT6336U Time Period for enter to Monitor Mode: 0x");
-    Serial.println(ft6336u.read_time_period_enter_monitor(), HEX);
+    Serial.println(ft6336u.ft6336u.read_time_period_enter_monitor(), HEX);
     Serial.print("FT6336U Active Rate: 0x");
-    Serial.println(ft6336u.read_active_rate(), HEX);
+    Serial.println(ft6336u.ft6336u.read_active_rate(), HEX);
     Serial.print("FT6336U Monitor Rate: 0x");
-    Serial.println(ft6336u.read_monitor_rate(), HEX);
+    Serial.println(ft6336u.ft6336u.read_monitor_rate(), HEX);
 
     Serial.print("FT6336U LIB Ver: 0x");
-    Serial.println(ft6336u.read_library_version(), HEX);
+    Serial.println(ft6336u.ft6336u.read_library_version(), HEX);
     Serial.print("FT6336U Chip ID: 0x");
-    Serial.println(ft6336u.read_chip_id(), HEX);
+    Serial.println(ft6336u.ft6336u.read_chip_id(), HEX);
     Serial.print("FT6336U G Mode: 0x");
-    Serial.println(ft6336u.read_g_mode(), HEX);
+    Serial.println(ft6336u.ft6336u.read_g_mode(), HEX);
     Serial.print("FT6336U POWER Mode: 0x");
-    Serial.println(ft6336u.read_pwrmode(), HEX);
+    Serial.println(ft6336u.ft6336u.read_pwrmode(), HEX);
     Serial.print("FT6336U Firm ID: 0x");
-    Serial.println(ft6336u.read_firmware_id(), HEX);
+    Serial.println(ft6336u.ft6336u.read_firmware_id(), HEX);
     Serial.print("FT6336U Focal Tehc ID: 0x");
-    Serial.println(ft6336u.read_focaltech_id(), HEX);
+    Serial.println(ft6336u.ft6336u.read_focaltech_id(), HEX);
     Serial.print("FT6336U Release Code ID: 0x");
-    Serial.println(ft6336u.read_release_code_id(), HEX);
+    Serial.println(ft6336u.ft6336u.read_release_code_id(), HEX);
     Serial.print("FT6336U State: 0x");
-    Serial.println(ft6336u.read_state(), HEX);
+    Serial.println(ft6336u.ft6336u.read_state(), HEX);
 
 }
 
 FT6336U_TouchPointType tp;
 void loop() {
-    tp = ft6336u.scan();
+    tp = ft6336u.ft6336u.scan();
     char tempString[128];
     sprintf(tempString, "FT6336U TD Count %d / TD1 (%d, %4d, %4d) / TD2 (%d, %4d, %4d)\r", tp.touch_count, tp.tp[0].status, tp.tp[0].x, tp.tp[0].y, tp.tp[1].status, tp.tp[1].x, tp.tp[1].y);
     Serial.print(tempString);
